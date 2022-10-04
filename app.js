724fc21bd1adf6d840089389e3d7f0b2052a09b6
Nicolle <fajoce@gmail.com>
2022-10-04 08:45:42 -0500

    func peso a dolar

---
+++ app.js
@@ -0,0 +1,12 @@
+function imc(p,a){
+    return Math.pow(p/a,2);
+}
+function suma(x,y){
+    return x + y;
+}
+
+function cambio(peso){
+return peso / 4500;
+}
+console.log(suma(10,8));
+console.log(cambio(120000));
